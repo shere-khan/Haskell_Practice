6fd5e034dd1efe1e4f17cff21f66ef4bdff1247c
Barry <BarryJ@sra.com>
2016-08-11 13:42:54 -0400

    added code to parser.hs

---
+++ src/Parser.hs
@@ -0,0 +1,24 @@
+type Parser a = String -> [(a, String)]
+
+return' :: a -> Parser a
+return' v = \inp -> [(v, inp)]
+
+failure' :: Parser a
+failure' = \inp -> []
+
+item' :: Parser Char
+item' = \inp -> case inp of
+                     [] -> []
+                     (x:xs) -> [(x, xs)]
+
+parse' :: Parser a-> String -> [(a, String)]
+parse' p inp = p inp
+
+(>>>=) :: Parser a -> (a -> Parser b) -> Parser b
+p >>>= f = \inp -> case parse' p inp of
+                      [] -> []
+                      [(v, out)] -> parse' (f v) out
+
+item' >>>= \x ->
+item' >>>= \y ->
+return' (x, y)
--- src/practice2.hs
+++ src/practice2.hs
@@ -49,3 +49,27 @@ sumsqreven ns = sum . map (^2) . filter even
 
 twice :: (b -> b) -> b -> b
 twice f = f . f
+
+add' :: Int -> (Int -> Int)
+add' = \x -> (\y -> x + y)
+
+type Parser a = String -> [(a, String)]
+
+return' :: a -> Parser a
+return' v = \inp -> [(v, inp)]
+
+failure' :: Parser a
+failure' = \inp -> []
+
+item' :: Parser Char
+item' = \inp -> case inp of
+                     [] -> []
+                     (x:xs) -> [(x, xs)]
+
+parse' :: Parser a-> String -> [(a, String)]
+parse' p inp = p inp
+
+(##) :: Parser a -> (a -> Parser b) -> Parser b
+p ## f = \inp -> case parse' p inp of
+                      [] -> []
+                      [(v, out)] -> parse' (f v) out
