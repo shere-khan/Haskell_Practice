dbf56024debf717b82b08f7a0a72cc936594ca97
Justin <justin.barry.e@gmail.com>
2019-02-16 01:35:18 -0500

    Player rotation complete

--- src/sokoban.hs
+++ src/sokoban.hs
@@ -2,21 +2,26 @@
 import CodeWorld
 
 -- Defines types of picutres in game.
-data Tile = Wall | Ground | Storage | Box | Blank | Player deriving (Eq)
+data Tile = Wall | Ground | Storage | Box | Blank |
+	UpPlayer | DownPlayer | LeftPlayer | RightPlayer deriving (Eq)
 
-data Direction = R | U | L | D | None
+data Direction = R | U | L | D | None deriving (Eq)
 
 data Coord = C Integer Integer
 
 data Orientation = O Direction Coord
 
 -- Gives values to pictures.
-wall, ground, storage, box, player :: Picture
-wall    = colored (grey 0.4) (solidRectangle 1 1)
-ground  = colored yellow (solidRectangle 1 1)
-box     = colored brown (solidRectangle 1 1)
-player  = arrow & (colored purple (solidCircle 0.3)) & ground
-storage = solidCircle 0.3 &  ground
+wall, ground, storage, box :: Picture
+wall         = colored (grey 0.4) (solidRectangle 1 1)
+ground       = colored yellow (solidRectangle 1 1)
+box          = colored brown (solidRectangle 1 1)
+player       = arrow & (colored purple (solidCircle 0.3)) & ground
+upPlayer     = rotated (pi/2) player
+downPlayer   = rotated (pi) player
+downPlayer   = rotated (3*pi/2) player
+rightPlayer  = player
+storage      = solidCircle 0.3 &  ground
 
 drawTile :: Tile -> Picture
 drawTile Wall    = wall
@@ -24,7 +29,15 @@ drawTile Ground  = ground
 drawTile Storage = storage
 drawTile Box     = box
 drawTile Blank   = blank
-drawTile Player  = player
+drawTile UpPlayer  = upPlayer
+drawTile DownPlayer = downPlayer
+drawTile LeftPlayer = leftPlayer
+drawTile RightPlayer = rightPlayer
+
+arrow :: Picture
+arrow = translated (0.5) 0 $ rotated (pi/2) $ (piece1 & piece2)
+          where piece1 = rotated (pi/10) $ path [(0,0),(0,1)]
+                piece2 = rotated (-pi/10) $ path [(0,0),(0,1)]
 
 pictureOfMaze :: Picture
 pictureOfMaze = draw21times (\r -> draw21times (\c -> drawTileAt (O None (C r c))))
@@ -49,37 +62,24 @@ adjacentCoord (O U (C x y)) = (O U (C x (y+1)))
 adjacentCoord (O L (C x y)) = (O L (C (x-1) y))
 adjacentCoord (O D (C x y)) = (O D (C x (y-1)))
 
-handleTime :: Double -> Orientation -> Orientation
-handleTime _ o = o
-
 nextCoord :: Orientation -> Orientation
 nextCoord o
   | maze adj == Ground = adj
   | maze adj == Storage = adj
   | otherwise = o
       where adj = adjacentCoord o
-      
-handleEvent :: Event -> Orientation -> Orientation
-handleEvent (KeyPress key) o
-    | key == "Right"  = nextCoord o
-    | key == "Up"     = nextCoord o
-    | key == "Left"   = nextCoord o
-    | key == "Down"   = nextCoord o
-    | otherwise       = o
-handleEvent _ o       = o
-
-arrow :: Picture
-arrow = translated (0.5) 0 $ rotated (pi/2) $ (left & right)
-          where left = rotated (pi/10) $ path [(0,0),(0,1)]
-                right = rotated (-pi/10) $ path [(0,0),(0,1)]
 
 drawState :: Orientation -> Picture
 drawState o = (drawPlayer o) & pictureOfMaze
   
 drawPlayer :: Orientation -> Picture
-drawPlayer (O d (C x y)) =
-  if x == 0 && y == 0 then atCoord (C 0 1) (drawTile Player)
-  else atCoord (C x y) $ drawTile Player
+drawPlayer (O d (C x y))
+	| d == U = atCoord (C x y) $ drawTile UpPlayer
+	| d == L = atCoord (C x y) $ drawTile LeftPlayer
+	| d == D = atCoord (C x y) $ drawTile DownPlayer
+	| d == R = atCoord (C x y) $ drawTile RightPlayer
+	| otherwise = atCoord (C x y) $ drawTile UpPlayer
+
 
 maze :: Orientation -> Tile 
 maze (O d (C x y))
@@ -96,5 +96,19 @@ initialCoord = O R (C 0 1)
 exercise1 :: IO()
 exercise1 = interactionOf initialCoord handleTime handleEvent drawState
 
+------------------ EVENT HANDLING ---------------------------------------
+handleEvent :: Event -> Orientation -> Orientation
+handleEvent (KeyPress key) (O d c) 
+    | key == "Right"  = nextCoord (O R c)
+    | key == "Up"     = nextCoord (O U c)
+    | key == "Left"   = nextCoord (O L c)
+    | key == "Down"   = nextCoord (O D c)
+    | otherwise       = (O d c)
+handleEvent _ (O d c)       = (O d c)
+
+handleTime :: Double -> Orientation -> Orientation
+handleTime _ o = o
+
+------------------ MAIN ---------------------------------------
 main :: IO ()
 main = exercise1
