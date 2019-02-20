181317bc338de27b98f6f964979bb7d6eb90af71
Justin <justin.barry.e@gmail.com>
2019-02-20 12:50:09 -0500

    Current HW3
    Finished getting coords of box in list

---
+++ src/sokoban_hw3.hs
@@ -0,0 +1,209 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds -fno-warn-deprecations #-}
+import CodeWorld
+
+-- Lists
+
+data List a = Empty | Entry a (List a) deriving (Show)
+    
+mapList :: (a -> b) -> List a -> List b
+mapList _ Empty = Empty
+mapList f (Entry c cs) = Entry (f c) (mapList f cs)
+
+combine :: List Picture -> Picture
+combine Empty = blank
+combine (Entry p ps) = p & combine ps
+
+-- Coordinates
+
+data Coord = C Integer Integer | None deriving Show
+
+data Direction = R | U | L | D
+
+eqCoord :: Coord -> Coord -> Bool
+eqCoord = undefined
+
+adjacentCoord :: Direction -> Coord -> Coord
+adjacentCoord R (C x y) = C (x+1) y
+adjacentCoord U (C x y) = C  x   (y+1)
+adjacentCoord L (C x y) = C (x-1) y
+adjacentCoord D (C x y) = C  x   (y-1)
+
+moveFromTo :: Coord -> Coord -> Coord -> Coord
+moveFromTo = undefined
+
+-- The maze
+
+data Tile = Wall | Ground | Storage | Box | Blank
+       
+maze :: Coord -> Tile 
+maze (C x y)
+  | abs x > 4  || abs y > 4  = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x ==  2 && y <= 0        = Wall
+  | x ==  3 && y <= 0        = Storage
+  | x >= -2 && y == 0        = Box
+  | otherwise                = Ground
+
+noBoxMaze :: Coord -> Tile
+noBoxMaze = undefined            
+
+mazeWithBoxes :: List Coord -> Coord -> Tile
+mazeWithBoxes = undefined
+
+-- The state
+
+data State = State Coord Direction (List Coord)
+
+initialBoxes :: List Coord
+initialBoxes = undefined
+
+initialState :: State
+initialState = State (C 0 1) R (Entry (C 3 2) Empty)
+
+-- Event handling
+
+handleEvent :: Event -> State -> State
+handleEvent _ s = s -- FIXME!
+
+-- Drawing
+
+wall, ground, storage, box :: Picture
+wall =    colored (grey 0.4) (solidRectangle 1 1)
+ground =  colored yellow     (solidRectangle 1 1)
+storage = colored white      (solidCircle 0.3) & ground
+box =     colored brown      (solidRectangle 1 1)
+
+drawTile :: Tile -> Picture
+drawTile Wall    = wall
+drawTile Ground  = ground
+drawTile Storage = storage
+drawTile Box     = box
+drawTile Blank   = blank
+
+-- pictureOfMaze :: Picture
+-- pictureOfMaze = draw21times (\r -> draw21times (\c -> drawTileAt (C r c)))
+
+draw21times :: (Integer -> Picture) -> Picture
+draw21times something = go (-10)
+  where
+    go :: Integer -> Picture
+    go 11 = blank
+    go n  = something n & go (n+1)
+
+drawTileAt :: Coord -> Picture
+drawTileAt c = atCoord c (drawTile (maze c))
+
+
+atCoord :: Coord -> Picture -> Picture
+atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
+
+
+player :: Direction -> Picture
+player R = translated 0 0.3 cranium
+         & path [(0,0),(0.3,0.05)] 
+         & path [(0,0),(0.3,-0.05)] 
+         & path [(0,-0.2),(0,0.1)] 
+         & path [(0,-0.2),(0.1,-0.5)]
+         & path [(0,-0.2),(-0.1,-0.5)]
+  where cranium = circle 0.18
+                & sector (7/6*pi) (1/6*pi) 0.18
+player L = scaled (-1) 1 (player R) -- Cunning!
+player U = translated 0 0.3 cranium
+         & path [(0,0),(0.3,0.05)] 
+         & path [(0,0),(-0.3,0.05)] 
+         & path [(0,-0.2),(0,0.1)] 
+         & path [(0,-0.2),(0.1,-0.5)]
+         & path [(0,-0.2),(-0.1,-0.5)]
+  where cranium = solidCircle 0.18
+player D = translated 0 0.3 cranium
+         & path [(0,0),(0.3,-0.05)] 
+         & path [(0,0),(-0.3,-0.05)] 
+         & path [(0,-0.2),(0,0.1)] 
+         & path [(0,-0.2),(0.1,-0.5)]
+         & path [(0,-0.2),(-0.1,-0.5)]
+  where cranium = circle 0.18
+                & translated   0.06  0.08 (solidCircle 0.04)
+                & translated (-0.06) 0.08 (solidCircle 0.04)
+
+pictureOfBoxes :: List Coord -> Picture
+pictureOfBoxes cs = combine (mapList (\c -> atCoord c (drawTile Box)) cs)
+
+drawState :: State -> Picture
+drawState (State c d bc) = pictureOfMaze
+
+-- The complete interaction
+
+sokoban :: Interaction State
+sokoban = Interaction initialState (\_ c -> c) handleEvent drawState
+
+-- The general interaction type
+
+data Interaction world = Interaction
+        world
+        (Double -> world -> world)
+        (Event -> world -> world)
+        (world -> Picture)
+
+
+runInteraction :: Interaction s -> IO ()
+runInteraction (Interaction state0 step handle draw)
+  = interactionOf state0 step handle draw
+
+-- Resetable interactions
+
+resetable :: Interaction s -> Interaction s
+resetable (Interaction state0 step handle draw)
+  = Interaction state0 step handle' draw
+  where handle' (KeyPress key) _ | key == "Esc" = state0
+        handle' e s = handle e s
+
+-- Start screen
+
+startScreen :: Picture
+startScreen = scaled 3 3 (text "Sokoban!")
+
+data SSState world = StartScreen | Running world
+
+withStartScreen :: Interaction s  -> Interaction (SSState s)
+withStartScreen (Interaction state0 step handle draw)
+  = Interaction state0' step' handle' draw'
+  where
+    state0' = StartScreen
+    
+    step' _ StartScreen = StartScreen
+    step' t (Running s) = Running (step t s)
+    
+    handle' (KeyPress key) StartScreen | key == " " = Running state0
+    handle' _              StartScreen              = StartScreen
+    handle' e              (Running s)              = Running (handle e s)
+    
+    draw' StartScreen = startScreen
+    draw' (Running s) = draw s
+
+-- Get coordinates of boxes
+drawCol :: Integer -> Integer -> List Coord
+drawCol _ 10 = Empty
+drawCol 10 _ = Empty
+drawCol x y = case maze (C x y) of
+                Box -> (Entry (C x y) next)
+                t   -> next 
+  where next = drawCol x (y+1)
+
+drawRow :: Integer -> Integer -> List Coord
+drawRow _ 10 = Empty
+drawRow 10 _ = Empty
+drawRow x y  = appendList (drawCol x y) (drawRow (x + 1) y)
+
+appendList :: List Coord -> List Coord -> List Coord
+appendList k Empty = k
+appendList Empty k = k
+appendList (Entry a k) (Entry b l) = (Entry a (Entry b (appendList k l)))
+
+pictureOfMaze :: Picture
+pictureOfMaze = blank
+
+------------------------- MAIN FUNCTION -------------------------
+main :: IO ()
+-- main = runInteraction sokoban
+main = drawingOf $ solidCircle 3
